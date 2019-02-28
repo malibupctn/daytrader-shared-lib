b2ea8d3b18bb623d6df43f2c4c050367009e5cab
pctoolchain <39390095+pctoolchain@users.noreply.github.com>
2019-02-28 16:30:11 +0000

    Remove shell to use default

--- vars/spinnakerTrigger.groovy
+++ vars/spinnakerTrigger.groovy
@@ -1,5 +1,5 @@
 def call(containerName, appName, pipelineNameOrId) {
-  container(name: containerName, shell: '/busybox/sh') {
+  container(name: containerName) {
     withCredentials([string(credentialsId: 'spin-pipeline', variable: 'secretUrl')]) {
       sh "curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{}' '${secretUrl}/${appName}/${pipelineNameOrId}'"
     }
